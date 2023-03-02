cc07cfc38dfd257c10ed797dd034405781c3a9a6
Pierre Buclez <buclez.p@etoncollege.org.uk>
2023-03-02 17:43:53 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Pierre Buclez on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
